c8ad347520bcfe958a17facdf5206601b49d3276
AlexPaul518493 <paulalex626@gmail.com>
2026-09-10 16:48:33 +0100

    product

--- index.js
+++ index.js
@@ -1,25 +1,26 @@
-const express = require("express")
-const mongoose = require('mongoose');
-const userRoute = require("./routes/userRoutes.js")
+const express = require("express");
+const mongoose = require("mongoose");
+const userRouter = require("./routes/userRoutes.js")
+const productRoute = require("./routes/ProductRoutes.js")
+const compass_string = "mongodb://localhost:27017/cohort8_db";
+const atlas_string = "mongodb+srv://paulalex626_db_user:paulalex626@cluster0.v7guqo3"
 
-const compass_string = "mongodb://localhost:27017/cohort8_db"
-const atlas_string = "mongodb+srv://adinadev5_db_user:adinadev5_db_user@cluster0.k7dgx4z.mongodb.net/cohort8_db?appName=Cluster0"
-
-mongoose.connect(compass_string)
-    .then(() => console.log("MongoDB Connected"))
-    .catch(err => console.error("Connection Error: ", err));
-
-
-const app = express()
-const port = 5555
+mongoose.connect(compass_string,)
+.then(() => console.log("MongoDB Connected"))
+.catch((err) => console.error("Connection error: ", err));
 
 
-app.use(express.json())
+const app = express();          
+const port = 5555           
 
-app.get("/", (req, res) => {
-    res.send("server is active")
-})
-app.use("/users", userRoute)
+app.use(express.json());                
+app.use("/products", productRoute);
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+app.use("/users" , userRouter)
 app.listen(port, () => {
-    console.log(`server is up and running on port : ${port}`)
-})
+  console.log(`Server is running on port : ${port}`);
+});
+
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,8 +1,8 @@
 const express = require('express');
-const { uploadProduct, getAllProducts } = require('../controller/productController');
+const { uploadProduct, getAllProducts } = require('../controller/ProductController');
 
 const router = express.Router();
-router.post('/upload', uploadProduct);
+router.post('/upload/:userId', uploadProduct);
 router.get('/getall', getAllProducts);
 
 module.exports = router;
